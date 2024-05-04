bc4cc7a2f2467aad44fb24ab0a8f3aed46ef5e8c
Leonardo Medina <ext_lpmedina@falabella.cl>
2024-05-04 14:12:01 -0500

    day 4 DSA : classs 68

--- src/main/java/DSA_Dinesh_Varuani/README.md
+++ src/main/java/DSA_Dinesh_Varuani/README.md
@@ -1,2 +1,4 @@
 https://youtu.be/Q485CWk_QWk?list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&t=545
 https://www.youtube.com/watch?v=z0SOY9UlUPE&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=50
+https://youtu.be/u9igWutVc4o?list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&t=651
+https://www.youtube.com/watch?v=LWDVM_kgvdE&list=PL6Zs6LgrJj3tDXv8a_elC6eT_4R5gfX4d&index=68
--- src/main/java/DSA_Dinesh_Varuani/list/SinglyLinkedList.java
+++ src/main/java/DSA_Dinesh_Varuani/list/SinglyLinkedList.java
@@ -23,6 +23,15 @@ public class SinglyLinkedList {
             this.data = data;
             this.next = null;
         }
+        public void display() {
+            ListNode current = next;
+            while (current != null) {
+                System.out.print(current.data + " | ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+
     }
 
     public void display() {
@@ -131,42 +140,335 @@ public class SinglyLinkedList {
         return current;
     }
 
+    /**
+     * class 54, 55
+     * Q. Implement method to delete a node at a given position.
+     *
+     * Assuming position to be valid and starting from 1
+     */
+    public ListNode deleteAtPosition(int position) {
+        if (position == 1) {
+            ListNode temp = head;
+            head = head.next;
+            temp.next = null;
+            return temp;
+        } else {
+            ListNode previous = head;
+            int count = 1;
+            while (count < position - 1) {
+                previous = previous.next;
+                count++;
+            }
+            ListNode current = previous.next;
+            previous.next = current.next;
+            current.next = null;
+            return current;
+        }
+    }
+    /**
+     * class 56
+     * Q. Implement method to search for a key in a linked list
+     */
+    public boolean find(int key) {
+        ListNode current = head;
+        while (current != null) {
+            if (current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    /**
+     * class 57
+     * Q. Implement method to reverse a linked list
+     */
+    public ListNode reverse() {
+      if(head == null) {
+          return head;
+      }
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+    /**
+     * class 58
+     * Q. Implement method to find the middle node of a linked list
+     */
+    public ListNode findMiddle() {
+         if(head == null) {
+             return null;
+         }
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+
+    /**
+     * class 59
+     * Q. Implement method to find the nth node from the end of a linked list
+     */
+    public ListNode findNthFromEnd(int n) {
+        if (head == null) {
+            return null;
+        }
+        if (n <= 0) {
+            throw new IllegalArgumentException("Invalid value: n = " + n);
+        }
+        ListNode mainPtr = head; // main pointer
+        ListNode refPtr = head; // reference pointer
+        int count = 0;
+        while (count < n) {
+            if (refPtr == null) {
+                throw new IllegalArgumentException(n + " is greater than the number of nodes in the list");
+            }
+            refPtr = refPtr.next;
+            count++;
+        }
+        while (refPtr != null) {
+            refPtr = refPtr.next;
+            mainPtr = mainPtr.next;
+        }
+        return mainPtr;
+    }
+    /**
+     * class 60
+     * Q. Remove duplicate from sorted linked list
+     */
+    public void removeDuplicates() {
+        if (head == null) {
+            return;
+        }
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    /**
+     * class 61
+     * Q. Insert a node in a sorted linked list
+     *
+     * @return
+     */
+    public ListNode insertInSortedList(int value) {
+        ListNode newNode = new ListNode(value);
+        if (head == null) {
+            return newNode;
+        }
+        ListNode current = head;
+        ListNode temp = head;
+        while (current != null && current.data < newNode.data) {
+            temp = current;
+            current = current.next;
+        }
+        newNode.next = current.next;
+       temp.next = newNode;
+
+        return head;
+    }
+
+    /**
+     * class 62
+     * Q. Implement method to delete a node with a given key
+     */
+    public ListNode deleteNode(int key) {
+        ListNode current = head;
+        ListNode previous = null;
+        while (current != null && current.data != key) {
+            previous = current;
+            current = current.next;
+        }
+        if (current == null) {
+            return head;
+        }
+        previous.next = current.next;
+        return current;
+    }
+
+    /**
+     * class 63, 64
+     * Q. Implement method to delete a loop in a linked list
+     */
+    public boolean detectLoop() {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if (slowPtr == fastPtr) {
+//                System.out.println("Loop found");
+                return true;
+            }
+        }
+//        System.out.println("Loop not found");
+        return false;
+    }
+
+    /**
+     * class 65, 66
+     * Q. Implement method find stating point of a loop in a linked list
+     */
+    public ListNode findLoopStart() {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        boolean loopExists = false;
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if (slowPtr == fastPtr) {
+                loopExists = true;
+                break;
+            }
+        }
+        if (loopExists) {
+            ListNode temp = head;
+            while (temp != slowPtr) {
+                temp = temp.next;
+                slowPtr = slowPtr.next;
+            }
+            return temp;
+        }
+        return null;
+    }
+
+    /*
+        class 67 Why Floyd's Cycle Detection algorithm works
+     */
+
+    /**
+     * class 68
+     * Q. remove loop in a linked list
+     */
+
+
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
-        sll.head = new ListNode(10);
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(8);
-        ListNode fourth = new ListNode(11);
+        sll.head = new ListNode(4);
+        ListNode sec = new ListNode(5);
+        ListNode trh = new ListNode(6);
+        ListNode fourt = new ListNode(7);
         // Now we will connect them together to form a chain
-        sll.head.next = second; // 10 --> 1
-        second.next = third; // 10 --> 1 --> 8
-        third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
+        sll.head.next = sec; // 10 --> 1
+        sec.next = trh; // 10 --> 1 --> 8
+        trh.next = fourt; // 10 --> 1 --> 8 --> 11 --> null
 
-        sll.insertFirst(0);
-        sll.insertFirst(2);
         sll.insertFirst(3);
+        sll.insertFirst(2);
+        sll.insertFirst(1);
 
-        sll.insertLast(12);
-        sll.insertLast(13);
-        sll.insertLast(14);
+        sll.insertLast(8);
+        sll.insertLast(9);
+        sll.insertLast(10);
 
-        sll.insertAtPosition(15, 4);
-        sll.insertAtPosition(16, 5);
-        sll.insertAtPosition(12, 5);
-        sll.insertAtPosition(16, 13);
+//        sll.insertAtPosition(15, 4);
+//        sll.insertAtPosition(16, 5);
+//        sll.insertAtPosition(12, 5);
+//        sll.insertAtPosition(16, 13);
         sll.display();
         System.out.println("Length of the linked list is: " + sll.length());
 
         ListNode test = sll.deleteFirst();
-        System.out.println("Delete first: "+test.data);
+        System.out.println("\nDelete first: "+test.data);
         System.out.println("After deleting first node");
         sll.display();
 
         test = sll.deleteLast();
-        System.out.println("Delete last: "+test.data);
+        System.out.println("\nDelete last: "+test.data);
         System.out.println("After deleting last node");
         sll.display();
 
+        System.out.println("\nDelete a node fron a Single linked list at a given position".toUpperCase());
+        test = sll.deleteAtPosition(1);
+        System.out.println("Delete at position 1: "+test.data);
+        test = sll.deleteAtPosition(3);
+        System.out.println("Delete at position 3: "+test.data);
+        sll.display();
+
+        System.out.println("\nSearch an element in a single linked list in java".toUpperCase());
+        System.out.println("Is 8 present in the list: "+sll.find(8));
+        System.out.println("Is 20 present in the list: "+sll.find(20));
+
+        System.out.println("\nReverse a single linked list in java".toUpperCase());
+        sll.display();
+        ListNode test2 = sll.reverse();
+        sll.head = test2;
+        sll.display();
+
+        System.out.println("\nFind the middle node of a single linked list in java".toUpperCase());
+        ListNode middle = sll.findMiddle();
+        sll.display();
+        System.out.println("Middle node is: "+middle.data);
+
+        System.out.println("\nFind the nth node from the end of a single linked list in java".toUpperCase());
+        sll.display();
+
+        ListNode nthNode = sll.findNthFromEnd(3);
+        System.out.println("3rd node from the end is: "+nthNode.data);
+
+        System.out.println("\nRemove duplicates from a sorted linked list".toUpperCase());
+        sll.insertAtPosition(5, 5);
+        sll.insertAtPosition(9, 2);
+        sll.insertAtPosition(3, 8);
+        sll.display();
+        sll.removeDuplicates();
+        sll.display();
+
+        System.out.println("\nReverse for test insertInSortedList".toUpperCase());
+        sll.display();
+        ListNode test3 = sll.reverse();
+        sll.head = test3;
+        sll.display();
+
+        System.out.println("\nInsert a node in a sorted linked list".toUpperCase());
+        sll.insertInSortedList(5);
+        sll.insertInSortedList(2);
+        sll.display();
+
+        System.out.println("\nDelete a node with a given key".toUpperCase());
+        sll.display();
+        ListNode test4 = sll.deleteNode(5);
+        System.out.println("After deleting node with key 5");
+        sll.display();
+        test4 = sll.deleteNode(8);
+        System.out.println("After deleting node with key 8");
+        sll.display();
+
+        System.out.println("\nDetect a loop in a linked list".toUpperCase());
+        sll.head = new ListNode(1);
+       ListNode second = new ListNode(2);
+         ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+        sll.head = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third;// LOOP
+        //sll.display(); // infinite loop
+        System.out.println("Contains infinitive loop? "+sll.detectLoop());
+
+        System.out.println("\nDetect a stating point in loop in a linked list".toUpperCase());
+        System.out.println("starting point: "+sll.findLoopStart().data);
     }
 
 }
