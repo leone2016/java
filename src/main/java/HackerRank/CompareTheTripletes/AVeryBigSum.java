6cf5846e01a8d9ce67212f4f2dcda0fec12d40d4
Leonardo Medina <ext_lpmedina@falabella.cl>
2024-06-01 17:48:57 -0500

    easy: a very big sum

---
+++ src/main/java/HackerRank/CompareTheTripletes/AVeryBigSum.java
@@ -0,0 +1,46 @@
+package HackerRank.CompareTheTripletes;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+        // Write your code here
+        Long result = 0L;
+        for (Long i : ar) {
+            result += i;
+        }
+        return result;
+
+    }
+
+}
+public class AVeryBigSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
